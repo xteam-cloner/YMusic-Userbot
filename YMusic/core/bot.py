bb8849685f4c4b6fdc32b62ee3ea02648b19c334
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-04-06 13:09:06 +0700

    Update bot.py

--- YMusic/core/bot.py
+++ YMusic/core/bot.py
@@ -4,12 +4,13 @@ from pytgcalls import PyTgCalls
 import config
 from ..logging import LOGGER
 
-api_id: int = config.API_ID
-api_hash: str = config.API_HASH
-session_string: str = config.SESSION_STRING
-
-YMusicBot = Client(
-    name="YMusic", api_id=api_id, api_hash=api_hash, session_string=session_string
-)
-
-YMusicUser = PyTgCalls(YMusicBot)
+class YMusicBot(Client):
+    def __init__(self):
+        self.one = Client(
+            name="YMusic",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.SESSION_STRING),
+            no_updates=True,
+        )
+        YMusicUser = PyTgCalls(YMusicBot)
