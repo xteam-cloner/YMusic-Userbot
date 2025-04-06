c4ba03610888bbfa267e0886e0cd94daa40aafe3
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-04-06 14:04:10 +0700

    Update config.py

--- config.py
+++ config.py
@@ -21,13 +21,13 @@ BOT_USERNAME = getenv("BOT_USERNAME")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 240))
+DURATION_LIMIT_MIN = getenv("DURATION_LIMIT")
 
 # Chat id of a group for logging bot's activities
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
+LOG_GROUP_ID = getenv("LOG_GROUP_ID")
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = getenv("OWNER_ID")
 
 PREFIX = int(getenv("PREFIX", "!"))
 
