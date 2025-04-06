9969eab92cd8c5278a3d84e2f2ff6917e97c380f
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-04-06 13:11:03 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,31 @@
+import re
+import os
+from os import getenv
+from dotenv import load_dotenv
+from pyrogram import filters
+import sys
+from decouple import config
+
+load_dotenv()
+
+# Configuration using getenv and decouple config
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+# Get your token from @BotFather on Telegram.
+BOT_TOKEN = getenv("BOT_TOKEN")
+
+SESSION = config("SESSION")
+
+BOT_USERNAME = getenv("BOT_USERNAME")
+
+# Get your mongo url from cloud.mongodb.com
+MONGO_DB_URI = getenv("MONGO_DB_URI")
+
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 240))
+
+# Chat id of a group for logging bot's activities
+LOG_GROUP_ID = int(getenv("LOGGER_ID"))
+
+# Get this value from @FallenxBot on Telegram by /id
+OWNER_ID = int(getenv("OWNER_ID"))
