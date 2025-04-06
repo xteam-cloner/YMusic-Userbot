e25920d1042c0db2464251ceaff61cafa26154a6
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-04-06 13:55:23 +0700

    Update config.py

--- config.py
+++ config.py
@@ -30,9 +30,9 @@ LOG_GROUP_ID = int(getenv("LOGGER_ID"))
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PREFIX = int(getenv("PREFIX", "!")
+PREFIX = int(getenv("PREFIX", "!"))
 
-RPREFIX = int(getenv("RPREFIX", "$")
+RPREFIX = int(getenv("RPREFIX", "$"))
 
 
 # No Need To Edit Below This
